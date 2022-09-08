4244ea43356a9b3e28916127e99d30c2726cbc11
Ronan Doudiès <rondou@eptm-it.ch>
2022-09-08 14:59:18 +0200

    Update Script_Initialize_Switch.py

--- Script_Initialize_Switch.py
+++ Script_Initialize_Switch.py
@@ -4,14 +4,16 @@ import sys
 from time import sleep
 from serial.serialutil import Timeout
 
+com_port = "COM7"
+
 def send_to_console(ser: serial.Serial, command: str, wait_time: float = 1):
     """
-    Fonction pour paramétrer l'envoi des commandes à l'appareil via un port série
+    Function to send the commands to the serial port
 
     Args:
-        ser: Connexion au port série
-        command: string, défini la commande qui sera envoyée via le port série
-        wait_time: float, temps d'attente après avoir exécuter la commande
+        ser: Connecting to serial port
+        command: string, The command that will be sent to the serial port
+        wait_time: float, wait time after executing the command
     """
     command_to_send = command + "\r"
     ser.write(command_to_send.encode('utf-8'))
@@ -20,89 +22,95 @@ def send_to_console(ser: serial.Serial, command: str, wait_time: float = 1):
 
 def configure_switch():
 
-    print("Configuration Interface VLAN1")
-    """     Fonction pour passer les commandes à l'appareil     """
-    ser = serial.Serial('COM7', timeout=1)
-    print(f"Connexion au port {ser.name}...")
+    print("Configure VLAN1")
+    ser = serial.Serial(com_port, timeout=1)
+    print(f"Connecting to port {ser.name}...")
 
-    #Passage en mode configuration
+    #Switch to configuration mode
     send_to_console(ser, "enable")
     send_to_console(ser, "conf t")
 
-    # Configuration VLAN1 pour management
+    # Configure VLAN1
     send_to_console(ser, "interface vlan 1")
     send_to_console(ser, "ip address 192.168.10.10 255.255.255.0")
     send_to_console(ser, "no shutdown")
     send_to_console(ser, "exit")
 
-    # Fermeture de la connexion série
+    # Close serial connexion 
     ser.close()
 
 def tftp_transfer():
     
-    print("Récupération fichier .tar")
-    """     Fonction pour passer les commandes à l'appareil     """
-    ser = serial.Serial('COM7', timeout=1)
+    print("Download .tar file")
+    ser = serial.Serial(com_port, timeout=1)
 
-    print(f"Connexion au port {ser.name}...")
+    print(f"Connecting to port {ser.name}...")
 
-    #Passage en mode configuration
+    #Switch to configuration mode
     send_to_console(ser, "end")
     send_to_console(ser, "enable")
+
+    #Get the new tar file via TFTP and extract it to the flash directory
     send_to_console(ser, "archive tar /xtract tftp://192.168.10.12/c1000-universalk9-tar.152-7.E6.tar flash: \r\n")
 
-    # Fermeture de la connexion série
+    # Close serial connexion
     ser.close()
 
 def boot_system():
 
-    print("Boot sur nouveau firmware")  
-    """     Fonction pour passer les commandes à l'appareil     """
-    ser = serial.Serial('COM7', timeout=1)
+    print("Boot the new firmware")  
+    ser = serial.Serial(com_port, timeout=1)
 
-    print(f"Connexion au port {ser.name}...")
+    print(f"Connecting to port {ser.name}...")
 
+    #Switch to configuration mode
     send_to_console(ser, "end")
     send_to_console(ser, "enable")
     send_to_console(ser, "conf t")
+
+    #Boot the new system firmware
     send_to_console(ser, "boot system flash:/c1000-universalk9-mz.152-7.E6/c1000-universalk9-mz.152-7.E6.bin")
     send_to_console(ser, "exit")
     send_to_console(ser, "reload")
     send_to_console(ser, "yes")
     send_to_console(ser, "\r\n")
 
-    # Fermeture de la connexion série
+    # Close serial connexion
     ser.close()
 
 def delete_directory():
 
-    print("Suppression anciens dossiers et fichiers")
-    """     Fonction pour passer les commandes à l'appareil     """
-    ser = serial.Serial('COM7', timeout=1)
-    print(f"Connexion au port {ser.name}...")
+    print("Delete old firmware files and folders")
+    ser = serial.Serial(com_port, timeout=1)
+    print(f"Connecting to port {ser.name}...")
 
+    #Switch to enable mode
     send_to_console(ser, "\r\n")
     send_to_console(ser, "enable")
+
+    #Delete the folders and files of the old firmwares
     send_to_console(ser, "delete /force /recursive flash:/c1000-universalk9-mz.152-7.E4")
     send_to_console(ser, "delete /force flash:/c1000-universalk9-tar.152-7.E4.tar")
     send_to_console(ser, "delete /force flash:/c1000-universalk9-mz.152-7.E6.bin")
     send_to_console(ser, "delete /force flash:/config.text")
     send_to_console(ser, "delete /force flash:/private-config.text")
     send_to_console(ser, "exit")
+
+    #Reboot the switch
     send_to_console(ser, "reload")
 
-    # Fermeture de la connexion série
+    # Close serial connexion
     ser.close()
 
 def timer(seconds):
 
     for remaining in range(seconds, 0, -1):
         sys.stdout.write("\r")
-        sys.stdout.write("{:2d} secondes restantes".format(remaining))
+        sys.stdout.write("{:2d} seconds remaining".format(remaining))
         sys.stdout.flush()
         time.sleep(1)
 
-    sys.stdout.write("\rFini !\n")
+    sys.stdout.write("\rDone !\n")
 
 configure_switch()
 tftp_transfer()
